50303e32700f53209e23b44a23f5e69738029b52
Adam Hany <adam.afify13@gmail.com>
2026-04-05 00:11:08 +0200

    ... This is getting tedious. RemapPic done

--- C++/libs/IDT.cpp
+++ C++/libs/IDT.cpp
@@ -8,7 +8,7 @@ struct IDT64 {
     uint_8 ist;
     uint_8 types_attr;
     uint_16 offset_mid;
-    uint_32 offset_hugh;
+    uint_32 offset_high;
     uint_32 zero;
 };
 
@@ -18,7 +18,17 @@ extern IDT64 _idt[256];
 extern uint_64 isr1;
 
 void InitializeIDT(){
+    _idt[1].zero = 0;
+    _idt[1].offset_low = (uint_16)((uint_64)& isr1 & 0xFFFF);
+    _idt[1].offset_mid = (uint_16)(((uint_64)& isr1 & 0xFFFF) >> 16);
+    _idt[1].offset_high = (uint_16)(((uint_64)& isr1 & 0xFFFF) >> 32);
+    _idt[1].ist = 0;
+    _idt[1].selector = 0x08;
+    _idt[1].selector = 0x8e;
+    RemapPic();
+
     
+
     LoadIDT();
 }
 
