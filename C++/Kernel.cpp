a883c271674236d1fe68b8ba4772481a6104a365
Adam Hany <adam.afify13@gmail.com>
2026-04-06 21:36:00 +0200

    Finally. Custom Keyboard Handler

--- C++/Kernel.cpp
+++ C++/Kernel.cpp
@@ -2,9 +2,17 @@
 #include "libs/IDT.cpp"
 
 extern const char Test[];
+bool LShift = false;
+
 
 void KeyboardHandler(uint_8 scanCode, uint_8 chr){
     if (scanCode < 0x80 and chr != 0){
+        switch (LShift){
+            case true:
+                PrintChar(KBSet1::ShiftScanCodeLookupTable[scanCode]);
+            case false:
+                PrintChar(chr);
+        }
         PrintChar(chr);
     } else {
         switch (scanCode){
@@ -12,6 +20,15 @@ void KeyboardHandler(uint_8 scanCode, uint_8 chr){
                 SetCursorPosition(CursorPosition - 1);
                 PrintChar(' ');
                 SetCursorPosition(CursorPosition - 1);
+                break;
+            case 0x2A:
+                LShift = true;
+                break;
+            case 0xAA:
+                LShift = false;
+                break;
+            
+
 
         }
     }
