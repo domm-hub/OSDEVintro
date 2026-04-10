39f0275f7222560354afe83df68c4292b60c3f13
Adam Hany <adam.afify13@gmail.com>
2026-04-10 16:19:21 +0200

    HeapingHeaps

--- C++/Kernel.cpp
+++ C++/Kernel.cpp
@@ -4,17 +4,32 @@
 #include "MemoryMap.h"
 #include "Heap.h"
 
+void println(const char* str, uint_8 clr = 0x0F){
+    PrintString(str, clr);
+    PrintString("\n", clr);
+}
+
+
 extern "C" void _start(){
     uint_8 clr = BACKGROUND_BLACK | FOREGROUND_WHITE;
     InitializeIDT();
+    PrintString("Hello Guys!\n");
     MainKeyboardHandler = KeyboardHandler;
 
     MemoryMapEntry** UsableMemoryMaps= GetUsableMemoryRegions();
 
-    for (uint_8 i = 0; i < UsableMemoryRegionsCount; i++){
-        MemoryMapEntry* memMap = UsableMemoryMaps[i];
-        PrintMemoryMap(memMap, CursorPosition);
-    }
+    InitializeHeap(0x100000, 0x100000);
+    void* testmem = malloc(0x10);
+    void* testmem2 = malloc(0x10);
+    void* testmem3 = malloc(0x10);
+
+
+    PrintString("0x");
+    println(HexToString((uint_64)testmem));
+    println(HexToString((uint_64)testmem2));
+    println(HexToString((uint_64)testmem3));
+
+
 
 }
 
